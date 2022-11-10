089e21a63ad06f4b240dd7740decf67686e1c086
Sammy Mureithi <sammymureithi20@gmail.com>
2022-11-10 17:03:00 +0300

    Home

---
+++ Components/GasSummery.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import {Image, Text, View} from 'react-native';
+import {Card} from 'react-native-paper';
+function GasSummery({volume, percentG, percentE, Gas, Empties}) {
+  return (
+    <Card style={{width: '90%', marginTop: 20}}>
+      <Card.Content>
+        <View>
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}>
+            <Text style={{color: 'black', fontWeight: 'bold'}}>{volume}</Text>
+            <Image
+              source={require('../Images/Logo.jpeg')}
+              style={{width: 50, height: 50}}
+            />
+          </View>
+          <View style={{width: '100%', display: 'flex', flexDirection: 'row'}}>
+            <View
+              style={{
+                height: 20,
+                backgroundColor: 'green',
+                width: '68%',
+                borderBottomRightRadius: 50,
+              }}>
+              <Text style={{color: 'white'}}>{percentG}</Text>
+            </View>
+            <View
+              style={{
+                height: 20,
+                backgroundColor: 'red',
+                width: '32%',
+                borderBottomLeftRadius: 60,
+              }}>
+              <Text style={{color: 'white', left: 10}}>{percentE}</Text>
+            </View>
+          </View>
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+            }}>
+            <Text style={{color: 'black', fontWeight: '600'}}>Gas</Text>
+            <Text style={{color: 'black', fontWeight: '600'}}>Empties</Text>
+          </View>
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+            }}>
+            <Text>{Gas}</Text>
+            <Text>{Empties}</Text>
+          </View>
+        </View>
+      </Card.Content>
+    </Card>
+  );
+}
+
+export default GasSummery;
--- Screens/Home.js
+++ Screens/Home.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Image, Text, View} from 'react-native';
+import {Card} from 'react-native-paper';
+import GasSummery from '../Components/GasSummery';
 
 function Home() {
+  /**{volume,percentG,percentE,Gas,Empties} */
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Home</Text>
+      <GasSummery
+        volume={'6kg'}
+        percentG="68%"
+        percentE={'32%'}
+        Gas="170"
+        Empties="80"
+      />
+      <GasSummery
+        volume={'13kg'}
+        percentG="44%"
+        percentE={'56%'}
+        Gas="170"
+        Empties="80"
+      />
     </View>
   );
 }
