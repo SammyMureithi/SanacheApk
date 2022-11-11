b0430b5074fc22113f6a0a20b101ac28f742b69b
Sammy Mureithi <sammymureithi20@gmail.com>
2022-11-11 11:00:15 +0300

    HomeScreen

--- Components/HomeHeader.js
+++ Components/HomeHeader.js
@@ -1,37 +1,46 @@
 import React from 'react';
-import {Image, Text, View} from 'react-native';
+import {Image, Text, TouchableOpacity, View} from 'react-native';
 import {Card} from 'react-native-paper';
+import {Consumer, LoginContext} from '../Context/LoginContext';
 
 function HomeHeader() {
   return (
-    <Card
-      style={{
-        width: '100%',
-        height: '100%',
-        alignItems: 'center',
-      }}>
-      <Card.Content
-        style={{
-          width: '100%',
-          justifyContent: 'space-between',
-          display: 'flex',
-          padding: 0,
-          flexDirection: 'row',
-          alignItems: 'center',
-        }}>
-        <Image
-          source={require('../Images/dash.png')}
-          style={{marginLeft: 10, width: 30, height: 30}}
-        />
-        <Text style={{fontSize: 23, color: 'black', fontWeight: 'bold'}}>
-          DashBoard
-        </Text>
-        <Image
-          source={require('../Images/D.png')}
-          style={{width: 30, height: 30}}
-        />
-      </Card.Content>
-    </Card>
+    <Consumer>
+      {Context => {
+        return (
+          <Card
+            style={{
+              width: '100%',
+              height: '100%',
+              alignItems: 'center',
+            }}>
+            <Card.Content
+              style={{
+                width: '100%',
+                justifyContent: 'space-between',
+                display: 'flex',
+                padding: 0,
+                flexDirection: 'row',
+                alignItems: 'center',
+              }}>
+              <Image
+                source={require('../Images/dash.png')}
+                style={{marginLeft: 10, width: 30, height: 30}}
+              />
+              <Text style={{fontSize: 23, color: 'black', fontWeight: 'bold'}}>
+                DashBoard
+              </Text>
+              <TouchableOpacity onPress={Context.changeToken}>
+                <Image
+                  source={require('../Images/D.png')}
+                  style={{width: 30, height: 30}}
+                />
+              </TouchableOpacity>
+            </Card.Content>
+          </Card>
+        );
+      }}
+    </Consumer>
   );
 }
 
